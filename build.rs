63d49b1f7ee467e1139b36ffed0a98e1c5608d14
kungfoo <falconbmscontrol@gmail.com>
2025-09-29 19:47:14 +0200

    Only regenerate bindings if it's actually needed

--- build.rs
+++ build.rs
@@ -3,10 +3,17 @@ extern crate cmake;
 
 use cmake::Config;
 use std::env;
+use std::fs;
 use std::path::PathBuf;
 
 fn main() {
     let target = env::var("TARGET").unwrap();
+
+    // Tell Cargo when to rerun the build script
+    println!("cargo:rerun-if-changed=wrapper.h");
+    println!("cargo:rerun-if-changed=vendor/enet/include/");
+
+    // Generate bindings
     let bindings = bindgen::Builder::default()
         .clang_arg("-Ivendor/enet/include/")
         .header("wrapper.h")
@@ -18,18 +25,31 @@ fn main() {
         .expect("Unable to generate bindings");
 
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
+    let bindings_path = out_path.join("bindings.rs");
 
+    // Only write bindings if contents changed
+    let new_contents = bindings.to_string();
+    let write_needed = match fs::read_to_string(&bindings_path) {
+        Ok(existing_contents) => existing_contents != new_contents,
+        Err(_) => true,
+    };
+
+    if write_needed {
+        fs::write(&bindings_path, new_contents).expect("Couldn't write bindings!");
+    }
+
+    // Build C library
     let dst = Config::new("vendor/enet").build();
+    eprintln!("Built ENet to: {}", dst.display());
 
-    eprintln!("LUL: {}", dst.display());
-
+    // Link platform-specific libraries
     if target.contains("windows") {
         println!("cargo:rustc-link-lib=dylib=winmm");
     }
 
-    println!("cargo:rustc-link-search=native={}/lib/static", dst.display());
+    println!(
+        "cargo:rustc-link-search=native={}/lib/static",
+        dst.display()
+    );
     println!("cargo:rustc-link-lib=static=enet");
 }
